d7313846a081424208917f73213b363be5d10454
nadil11 <nadilnidhal111@gmail.com>
2023-11-16 12:31:04 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
+import morgan from "morgan"
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("common"))
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 dotenv.config();
 
